42c513a288711f959fc3d7458a2d9fd3c8adf5d0
Gabriel Magalhães <162460130+Gabrielsm88@users.noreply.github.com>
2025-11-19 15:44:37 -0300

    Update backend.tf

--- terraform/grafana/backend.tf
+++ terraform/grafana/backend.tf
@@ -1,6 +1,6 @@
   terraform{
     backend "s3" {
-        bucket = "chronyka-monitoring-state" # O mesmo bucket
+        bucket = "chronyka-monitoring-state2" # O mesmo bucket
         key    = "dummy"   # Chave ÚNICA para o QA
         region = "us-east-1"
         encrypt = true
