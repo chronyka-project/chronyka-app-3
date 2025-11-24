eb5c3c117fc030716892672fbcd1d64418c007be
Matheus Souza <matheus-teste@teste.com.br>
2025-11-24 16:55:57 -0300

    Update Terraform grafana

--- terraform/grafana/dev/main.tf
+++ terraform/grafana/dev/main.tf
@@ -16,7 +16,7 @@ provider "grafana" {
 # 2. Recurso: Fonte de Dados Prometheus (Garante que ela exista)
 resource "grafana_data_source" "prometheus" {
   type        = "prometheus"
-  name        = var.datasource_name
+  name        = "prometheus-${var.environment}-${var.app_name}"
   url         = "http://localhost:9090" # Assumindo que Prometheus roda na mesma VM que o Grafana, na porta 9090
   is_default  = false
   access_mode = "proxy"
--- terraform/grafana/qa/variables.tf
+++ terraform/grafana/qa/variables.tf
@@ -19,3 +19,8 @@ variable "environment" {
   description = "Nome do ambiente (dev, qa, prod)"
   type        = string
 }
+
+variable "app_name" {
+  description = "Nome da aplicação"
+  type        = string
+}
