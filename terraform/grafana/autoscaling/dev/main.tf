3a7e6b8f8d6c87c9a31d6d10ec99490388e07abd
matheus <matheus-teste@teste.com.br>
2025-11-27 10:42:27 -0300

    Arquivos para AutoScaling

---
+++ terraform/grafana/autoscaling/dev/main.tf
@@ -0,0 +1,37 @@
+# 1. Configuração do Provedor Grafana
+terraform {
+  required_providers {
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 2.15"
+    }
+  }
+}
+
+provider "grafana" {
+  url  = var.grafana_url
+  auth = var.grafana_api_key
+}
+
+# 2. Recurso: Fonte de Dados Prometheus (Garante que ela exista)
+resource "grafana_data_source" "prometheus" {
+  type        = "prometheus"
+  name        = "prometheus-${var.environment}-${var.app_name}"
+  url         = "http://localhost:9090"
+  is_default  = false
+  access_mode = "proxy"
+}
+
+# Local para gerar o JSON do Dashboard dinamicamente
+locals {
+  dashboard_config = jsondecode(
+    templatefile("${path.module}/../../dashboards/todo_metrics.json.tftpl", {
+      environment = var.environment
+    })
+  )
+}
+
+resource "grafana_dashboard" "todo_app_dashboard" {
+  overwrite   = true
+  config_json = jsonencode(local.dashboard_config)
+}
